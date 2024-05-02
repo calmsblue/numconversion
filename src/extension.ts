4771614b60d67877f6b464962f24e202d47e8849
adeu <calmblue2020@gmail.com>
2024-05-02 22:53:12 +0900

    引数を取れるようにした
    Co-authored-by: 2cobracda <2cobracda@users.noreply.github.com>

--- src/extension.ts
+++ src/extension.ts
@@ -13,10 +13,19 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('numconversion.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('numconversion.helloWorld', async () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from numconversion!');
+		const value = await vscode.window.showInputBox({
+			prompt: '引数を入力してください',
+			placeHolder: '例: hello world'
+		});
+
+		if(typeof value == "string"){
+			const getnum = parseInt(value);
+			vscode.window.showInformationMessage('Hello World from numconversion!:' + getnum.toString(16));
+		}
+
 	});
 
 	context.subscriptions.push(disposable);
